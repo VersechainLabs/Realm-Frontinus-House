c8eb78fb7e5967ba2f175134306a82aa06816e7c
Chao Chen <chaochen83@users.noreply.github.com>
2023-07-12 10:58:50 +0800

    delegate table add nomineeCount

--- packages/prop-house-backend/src/db/migrations/1688713778584-AddDelegates.ts
+++ packages/prop-house-backend/src/db/migrations/1688713778584-AddDelegates.ts
@@ -4,7 +4,19 @@ export class AddDelegates1688713778584 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `CREATE TABLE "delegate" ("id" SERIAL NOT NULL, "visible" boolean NOT NULL DEFAULT true, "title" character varying NOT NULL, "description" character varying, "startTime" TIMESTAMP NOT NULL, "proposalEndTime" TIMESTAMP NOT NULL, "votingEndTime" TIMESTAMP NOT NULL, "endTime" TIMESTAMP NOT NULL, "createdDate" TIMESTAMP NOT NULL, "lastUpdatedDate" TIMESTAMP, CONSTRAINT "PK_zxouifdiou29871287asfwiuar" PRIMARY KEY ("id"))`,
+            `CREATE TABLE "delegate" (
+                "id" SERIAL NOT NULL, 
+                "visible" boolean NOT NULL DEFAULT true, 
+                "title" character varying NOT NULL, 
+                "description" character varying, 
+                "nomineeCount" integer NOT NULL DEFAULT '0',
+                "startTime" TIMESTAMP NOT NULL, 
+                "proposalEndTime" TIMESTAMP NOT NULL, 
+                "votingEndTime" TIMESTAMP NOT NULL, 
+                "endTime" TIMESTAMP NOT NULL, 
+                "createdDate" TIMESTAMP NOT NULL, 
+                "lastUpdatedDate" TIMESTAMP, 
+                CONSTRAINT "PK_zxoui231asdfs12287asfwiuar" PRIMARY KEY ("id"))`,
           );
     }
 
