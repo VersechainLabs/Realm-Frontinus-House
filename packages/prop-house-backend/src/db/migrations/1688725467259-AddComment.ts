5becee0c63575be311e7e300b7abe990f0dc4d32
Chao Chen <chaochen83@users.noreply.github.com>
2023-07-13 10:59:19 +0800

    update comment migration

--- packages/prop-house-backend/src/db/migrations/1688725467259-AddComment.ts
+++ packages/prop-house-backend/src/db/migrations/1688725467259-AddComment.ts
@@ -1,10 +1,19 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class AddComment1688725467259 implements MigrationInterface {
+    name = 'AddComment1688725467259';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `CREATE TABLE "comment" ("id" SERIAL NOT NULL, "visible" boolean NOT NULL DEFAULT true, "owner" character varying NOT NULL, "content" character varying NOT NULL, "proposalId" integer NOT NULL,  "createdDate" TIMESTAMP NOT NULL, "lastUpdatedDate" TIMESTAMP, CONSTRAINT "PK_9172zdpaoureaueaouds123adf" PRIMARY KEY ("id"))`,
+            `CREATE TABLE "comment" (
+                "id" SERIAL NOT NULL, 
+                "visible" boolean NOT NULL DEFAULT true, 
+                "owner" character varying NOT NULL, 
+                "content" character varying NOT NULL, 
+                "proposalId" integer NOT NULL,  
+                "createdDate" TIMESTAMP NOT NULL, 
+                "lastUpdatedDate" TIMESTAMP, 
+                CONSTRAINT "PK_9171zsdpaourea1eaouds123adf" PRIMARY KEY ("id"))`,
           );        
     }
 
