b4e0fdac1a77c95497535e4451f00443698b5ff3
Chao Chen <chaochen83@users.noreply.github.com>
2023-07-14 15:22:34 +0800

    add missing files

---
+++ packages/prop-house-backend/src/admin/admin.controller.ts
@@ -0,0 +1,42 @@
+import {
+    Body,
+    Controller,
+    Get,
+    HttpException,
+    HttpStatus,
+    Param,
+    ParseIntPipe,
+    Post,
+    Query,
+  } from '@nestjs/common';
+  import { ParseDate } from 'src/utils/date';
+  import { Admin } from './admin.entity';
+  import { AdminService} from './admin.service';
+  import { ProposalsService } from 'src/proposal/proposals.service';
+  import { Proposal } from 'src/proposal/proposal.entity';
+  import { InfiniteAuctionProposal } from 'src/proposal/infauction-proposal.entity';
+  import { Order } from 'src/utils/dto-types';
+import { ECDSAPersonalSignedPayloadValidationPipe } from '../entities/ecdsa-personal-signed.pipe';
+import { CreateAdminDto } from './admin.types';
+  
+  @Controller('admins')
+  export class AdminsController {
+    [x: string]: any;
+    constructor(
+      private readonly adminService: AdminService,
+      private readonly proposalService: ProposalsService,
+    ) {}
+  
+    @Get('/list')
+    getAll(): Promise<Admin[]> {
+      return this.adminService.findAll(); 
+    }
+  
+
+    @Post('/create')
+    async create(@Body() createAdminDto: CreateAdminDto): Promise<Admin> {
+        return await this.adminService.createAdmin(createAdminDto);
+    }
+  
+
+  }
