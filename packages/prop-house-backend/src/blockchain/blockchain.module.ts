b1ee57560e52f032636538dc6f0f83b7daf93db2
liuwei <linvan6v@gmail.com>
2023-07-20 15:39:06 +0800

    add blockchain service

---
+++ packages/prop-house-backend/src/blockchain/blockchain.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { BlockchainService } from './blockchain.service';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  providers: [BlockchainService],
+})
+export class BlockchainModule {
+}
