edaea6bdfd3a79722ab74462b3b925ae4494b7d5
felix fan <felix@tapatalk.com>
2023-08-07 11:55:24 +0800

    fix issues

--- packages/frontinus-house-webapp/src/components/Comments/index.tsx
+++ packages/frontinus-house-webapp/src/components/Comments/index.tsx
@@ -31,6 +31,7 @@ export default function Comments(props: CommentsProps) {
   const [showLoadMore, setShowLoadMore] = useState(true);
   const [loading, setLoading] = useState(true);
 
+
   useEffect(() => {
     loadNextPage(0);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -63,10 +64,13 @@ export default function Comments(props: CommentsProps) {
           list = commentList.concat(res);
         }
 
-        if (list.length <= 0) {
+        if (res.length < 10) {
           setShowLoadMore(false);
         }
 
+        if( commentCount == 10 && skip == 0 ) {
+          setShowLoadMore(false);
+        }
         setCommentList(list);
       },
     ).finally(() => {
@@ -90,7 +94,7 @@ export default function Comments(props: CommentsProps) {
       itemList.push(CommentListItem({ comment: comment }));
     });
 
-    if (commentCount > 10 && (commentList.length % 10) === 0) {
+    if (showLoadMore) {
       itemList.push(
         <ListItem key={'has-more'} sx={{ justifyContent: 'center' }}>
           <LoadingButton
