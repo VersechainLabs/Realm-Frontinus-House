3164c0f6e888f97e51d02262b3b76eacf502b7e5
Chao Chen <chaochen83@users.noreply.github.com>
2023-08-08 11:15:47 +0800

    clean code

--- packages/frontinus-house-backend/src/delegate/delegate.types.ts
+++ packages/frontinus-house-backend/src/delegate/delegate.types.ts
@@ -44,31 +44,6 @@ export enum Order {
   DESC = 'DESC',
 }
 
-// export enum DelegateAPIResponses {
-//   OK = 'Success',
-//   NO_APPLICATION = 'Can not find application',
-//   NOT_VOTING = 'Not in the eligible voting period.',
-//   DELEGATED = 'Already delegate to another address.',
-//   OCCUPIED = 'Already created application. Can not delegate.',
-// }
-// export const DelegateAPIResponses = {
-//   OK : { Code: 20000, Detail: "Success"},
-//   NO_APPLICATION : { Code: 40031, Detail: "Can not find application"},
-//   NOT_VOTING : { Code: 40032, Detail: 'Not in the eligible voting period.'},
-//   DELEGATED : { Code: 40033, Detail: 'Already delegate to another address.'},
-//   OCCUPIED : { Code: 40034, Detail: 'Already created application. Can not delegate.'},
-// };
-// export const APIResponses = {
-//   OK : { Code: 20000, Detail: "Success"},
-//   DELEGATE : {
-//     NO_APPLICATION : { Code: 40031, Detail: "Can not find application"},
-//     NOT_VOTING : { Code: 40032, Detail: 'Not in the eligible voting period.'},
-//     DELEGATED : { Code: 40033, Detail: 'Already delegate to another address.'},
-//     OCCUPIED : { Code: 40034, Detail: 'Already created application. Can not delegate.'},
-//   }
-
-// };
-
 
 export class GetDelegateDto {
   @IsOptional()
