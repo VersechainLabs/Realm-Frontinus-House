85dccb811d35ca4ba26e75d297b2c49d47a18002
xfuhan <>
2023-08-15 16:33:01 +0800

    pop window modified

--- packages/frontinus-house-webapp/src/components/Popup/index.tsx
+++ packages/frontinus-house-webapp/src/components/Popup/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Popup.css';
+import classes from './Popup.module.css';
 
 interface PopupProps {
   trigger: boolean;
@@ -10,19 +10,19 @@ const Popup: React.FC<PopupProps> = (props: PopupProps) => {
   const { trigger, onClose } = props;
 
   return (
-    <div className="popup">
-      <div className="popup-inner">
+    <div className={classes.popup}>
+      <div className={classes.popupInner}>
         <div className={'frontinusTitle'}>
           Thank you for creating a round!
           <br />
           It will be posted as soon as the Admin approves it.
         </div>
 
-        <div className="button-group">
-          <button className="close-btn" onClick={onClose}>
+        <div className={classes.buttonGroup}>
+          <button className={classes.closeBtn} onClick={onClose}>
             <img src="/x-icon.png" alt="Close" />
           </button>
-          <button className="ok-btn" onClick={onClose}>
+          <button className={classes.okBtn} onClick={onClose}>
             OK
           </button>
         </div>
