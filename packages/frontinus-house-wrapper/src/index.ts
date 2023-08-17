16615172942d6904269f66e8b0afb18960c1142e
xfuhan <>
2023-08-17 15:39:42 +0800

    code modified

--- packages/frontinus-house-wrapper/src/index.ts
+++ packages/frontinus-house-wrapper/src/index.ts
@@ -97,6 +97,18 @@ export class ApiWrapper {
     }
   }
 
+  async getProposalApplied(id: number ): Promise<any> {
+    if (!this.signer) return;
+
+    try {
+      const owner = (await this.signer.getAddresses())[0];
+      const raw = (await axios.get(`${this.host}/proposals/canCreate?auctionId=${id}&address=${owner}`)).data;
+      return raw;
+    } catch (e: any) {
+      throw e.response.data.message;
+    }
+  }
+
   async getDelegateStatus(id: any): Promise<any> {
     if (!this.signer) throw 'Please sign';
     try {
