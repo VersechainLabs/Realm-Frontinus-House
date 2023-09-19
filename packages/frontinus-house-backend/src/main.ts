8051175194af835fffe6ee06f0da9dcd2025fef8
Chao Chen <chaochen83@users.noreply.github.com>
2023-09-19 15:37:04 +0800

    .env add API_PORT

--- packages/frontinus-house-backend/src/main.ts
+++ packages/frontinus-house-backend/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3001);
+  await app.listen(process.env.API_PORT);
 }
 
 bootstrap();
