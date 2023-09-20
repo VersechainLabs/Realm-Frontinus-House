5f63310f7ab2dea0772bf34687fd1a9b99a5ce4f
Chao Chen <chaochen83@users.noreply.github.com>
2023-09-20 15:29:45 +0800

    add sentry

--- packages/frontinus-house-backend/src/main.ts
+++ packages/frontinus-house-backend/src/main.ts
@@ -5,6 +5,8 @@ import * as bodyParser from 'body-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ParseBooleanPipe } from './entities/parse-boolean.pipe';
 import { GlobalExceptionFilter } from './filters/global-exception.filter';
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -25,6 +27,17 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new ProfilingIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  });
 
   await app.listen(process.env.API_PORT);
 }
