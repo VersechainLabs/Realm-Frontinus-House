1030c69c7c637f7a9eeef99fe15b6a1a50776214
Chao Chen <chaochen83@users.noreply.github.com>
2023-10-04 17:19:58 +0800

    revert

--- packages/frontinus-house-backend/src/delegate/delegate.controller.ts
+++ packages/frontinus-house-backend/src/delegate/delegate.controller.ts
@@ -228,9 +228,6 @@ export class DelegateController {
     // Start remove delegate.
     await this.delegateService.remove(foundDelegate.id);
 
-    // Update DB.application.delegatorCount:
-    await this.applicationService.updateDelegatorCount(foundApplication);
-
     return true;
   }
 
