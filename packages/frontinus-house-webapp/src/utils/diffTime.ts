e145d93f9eaed7dfc8a49a381ad4db1a22d633bd
chenglong <chenglong.wang@tapatalk.com>
2023-10-07 16:26:23 +0800

    fix

--- packages/frontinus-house-webapp/src/utils/diffTime.ts
+++ packages/frontinus-house-webapp/src/utils/diffTime.ts
@@ -1,7 +1,9 @@
-import dayjs from 'dayjs';
+// import dayjs from 'dayjs';
+import moment from 'moment';
 
 const diffTime = (time: Date | string) => {
-	return dayjs(time).fromNow()
+	// return dayjs(time).fromNow()
+	return moment(time).fromNow()
 }
 
 export default diffTime;
